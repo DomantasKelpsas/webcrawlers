9a55b303344f6a4727c041861e32376f2cf9b0c4
Domantas Kelpšas <domantas.kelpsas@gmail.com>
2025-02-12 15:44:44 +0200

    Add custom header

--- Rekvizitai/CsvHandler.py
+++ Rekvizitai/CsvHandler.py
@@ -10,17 +10,18 @@ class CsvHandler:
 
         try:
             csvfile = open(f'{Arguments.PATH_CSV}Rekvizitai_{sectionName}.csv', 'a',newline='',encoding='utf-8-sig')
+            fieldnames = ['CompanyName', 'phoneNumber1', 'phoneNumber2', 'phoneNumber3', 'employeeCount', 'revenue', 'profit', 'director', 'companyPage', 'companyAge', 'Address']
+            custom_headers = ['Įmonės pavadinimas', 'Mob. telefonas', 'Telefonas', 'Buh. telefonas', 'Darbuotojų skaičius', 'Pardavimo pajamos', 'Grynasis pelnas', 'Vadovas', 'Tinklalapis', 'Įmonės amžius', 'Adresas']
             
             with csvfile:
-                fieldnames = ['CompanyName', 'Address', 'phoneNumber1', 'phoneNumber2', 'phoneNumber3', 'employeeCount', 'revenue', 'profit', 'director', 'companyPage', 'companyAge']
                 writer = csv.DictWriter(csvfile,fieldnames=fieldnames,)
-                # writer.writeheader()
+                writer.writerow(dict(zip(fieldnames, custom_headers)))
                 for company in rekvizitai:
-                    writer.writerow({'CompanyName':company.companyName, 'Address':company.address,
+                    writer.writerow({'CompanyName':company.companyName,
                                      'phoneNumber1':company.phoneNumber1, 'phoneNumber2':company.phoneNumber2, 'phoneNumber3':company.phoneNumber3,
                                      'employeeCount':company.employeeCount,
                                      'revenue':company.revenue, 'profit':company.profit, 
-                                     'director':company.director, 'companyPage':company.companyPage, 'companyAge':company.companyAge,
+                                     'director':company.director, 'companyPage':company.companyPage, 'companyAge':company.companyAge, 'Address':company.address,
                                      })                                 
                         
         except Exception as e:
